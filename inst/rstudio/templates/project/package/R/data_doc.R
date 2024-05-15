58058861a09ab81fe9a7abb5ff4e06b1a26c3fb9
Su <coo@ssb.no>
2024-05-15 12:32:19 +0200

    Data useage change to fix R CMD check problem.

--- inst/rstudio/templates/project/package/R/data_doc.R
+++ inst/rstudio/templates/project/package/R/data_doc.R
@@ -2,11 +2,12 @@
 #'
 #' An example of how data is documented. See https://r-pkgs.org/data.html#sec-documenting-data
 #'
-#' @format ## `test_data`
-#' A data frame with 10 rows and 2 columns:
+#' @format A data frame with 10 rows and 2 columns:
 #' \describe{
 #'   \item{x}{Random variable}
 #'   \item{y}{Random variable}
 #' }
-#'  @export
+#' @usage data(test_data)
+#' @source Randomly generated using \code{stats::runif}
+#' @export
 "test_data"
